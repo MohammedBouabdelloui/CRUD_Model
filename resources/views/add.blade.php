ea5a3c03b7da9b9a3e8ce7f0a08a30215debb2b7
MOHAMMED BOUABDELLAOUI <118568976+SimoWoold@users.noreply.github.com>
2023-03-07 16:44:33 +0100

    add old value to input

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -16,11 +16,11 @@
         <div class="form-row">
             <div class="form-group col-md-6">
             <label for="inputEmail4">Email</label>
-            <input type="text" name="email" class="@error('email') is-invalid @enderror form-control" id="inputEmail4" placeholder="Email">
+            <input type="text" name="email" class="@error('email') is-invalid @enderror form-control" id="inputEmail4" placeholder="Email" value="{{old('email')}}">
             </div>
             <div class="form-group col-md-6">
             <label for="inputPassword4">Password</label>
-            <input type="password" name="password" class="@error('password') is-invalid @enderror form-control" id="inputPassword4" placeholder="Password">
+            <input type="password" name="password" class="@error('password') is-invalid @enderror form-control" value="{{old('password')}}" id="inputPassword4" placeholder="Password">
             </div>
         </div>
         <div class="form-row">
